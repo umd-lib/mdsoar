712393f7b2b19a75d0c7a16c32514870b444b6d2
David P. Steelman <dsteelma@umd.edu>
2024-01-17 11:01:49 -0500

    LIBCIR-368. Updated UmdSolrRebuildSuggestersScriptConfiguration for 7.6.1
    In DSpace 7.6.1 some previously abstract methods in ScriptConfiguration
    superclass used by UmdSolrRebuildSuggestersScriptConfiguration have
    been converted into concrete methods.
    
    Updated the UmdSolrRebuildSuggestersScriptConfiguration to remove
    code rendered unnecessary due to this change.
    
    https://umd-dit.atlassian.net/browse/LIBCIR-368

--- dspace/modules/additions/src/main/java/edu/umd/lib/dspace/content/authority/UmdSolrRebuildSuggestersScriptConfiguration.java
+++ dspace/modules/additions/src/main/java/edu/umd/lib/dspace/content/authority/UmdSolrRebuildSuggestersScriptConfiguration.java
@@ -1,32 +1,16 @@
 package edu.umd.lib.dspace.content.authority;
-import java.sql.SQLException;
 
 import org.apache.commons.cli.Options;
-import org.dspace.authorize.service.AuthorizeService;
-import org.dspace.core.Context;
 import org.dspace.scripts.configuration.ScriptConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class UmdSolrRebuildSuggestersScriptConfiguration<T extends UmdSolrRebuildSuggestersClient>
     extends ScriptConfiguration<T> {
-
-    @Autowired
-    private AuthorizeService authorizeService;
 
     private Class<T> dspaceRunnableClass;
 
     @Override
     public Class<T> getDspaceRunnableClass() {
         return dspaceRunnableClass;
-    }
-
-    @Override
-    public boolean isAllowedToExecute(Context context) {
-        try {
-            return authorizeService.isAdmin(context);
-        } catch (SQLException e) {
-            throw new RuntimeException("SQLException occurred when checking if the current user is an admin", e);
-        }
     }
 
     @Override
